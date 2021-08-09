8418c01b1de6c8eadfe53a52150b45fe5943e2cb
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-09 07:35:49 -0700

    chore: rename js to ts files

--- vite.esbuild.config.js
+++ vite.esbuild.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from "vite";
 // Refs: https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    include: [".js"],
     define: {
       global: "window",
     },
