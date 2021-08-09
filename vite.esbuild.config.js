8eaf73ccd9a8d65968a29e619b84b6ac81854078
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-09 07:31:16 -0700

    chore: include ".js" in vite esbuild config
    Refs:
    * https://github.com/vitejs/vite/issues/4548#issuecomment-895194916
    * https://github.com/vitejs/vite/issues/769#issuecomment-777843876

--- vite.esbuild.config.js
+++ vite.esbuild.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from "vite";
 // Refs: https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
+    include: [".js"],
+    exclude: [],
     define: {
       global: "window",
     },
