33f41205233918ab6812ad7ff8f5750f165e5877
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2021-08-10 13:58:06 -0700

    chore: fix for include/exclude config

--- vite.esbuild.config.js
+++ vite.esbuild.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from "vite";
 // Refs: https://vitejs.dev/config/
 export default defineConfig({
   esbuild: {
-    include: [".js"],
-    exclude: [],
+    include: [/\.js$/],
+    exclude: [/\.jsx$/],
     define: {
       global: "window",
     },
